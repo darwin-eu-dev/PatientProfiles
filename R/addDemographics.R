933d5952a8f34decff9cd5613ded0dc83a91855f
edward-burn <9583964+edward-burn@users.noreply.github.com>
2023-03-25 16:20:08 +0000

    addDemographics tidy up
    - Include options whether to return each characteristic
    - Include all age options in function
    - Pass tablePrefix to functions

--- R/addDemographics.R
+++ R/addDemographics.R
@@ -7,7 +7,20 @@
 #' for the individuals in the cohort
 #' @param demographicsAt name of the column with the date at which consider
 #' demographic information
+#' @param age  TRUE or FALSE. If TRUE, age will be calculated relative to
+#' demographicsAt
+#' @param ageDefaultMonth Month of the year assigned to individuals with missing
+#' month of birth. By default: 1.
+#' @param ageDefaultDay day of the month assigned to individuals with missing day
+#' of birth. By default: 1.
+#' @param ageImposeMonth Whether the month of the date of birth will be considered
+#' as missing for all the individuals. By default: TRUE.
+#' @param ageImposeDay Whether the day of the date of birth will be considered as
+#' missing for all the individuals. By default: TRUE.
 #' @param ageGroup if not NULL, a list of ageGroup vectors
+#' @param sex TRUE or FALSE. If TRUE, sex will be identified
+#' @param priorHistory TRUE or FALSE. If TRUE, days of prior history will
+#' be calculated relative to demographicsAt
 #' @param tablePrefix The stem for the permanent tables that will
 #' be created. If NULL, temporary tables will be used throughout.
 #'
@@ -27,11 +40,17 @@
 addDemographics <- function(x,
                         cdm,
                         demographicsAt = "cohort_start_date",
+                        age = TRUE,
+                        ageDefaultMonth = 1,
+                        ageDefaultDay = 1,
+                        ageImposeMonth = TRUE,
+                        ageImposeDay = TRUE,
                         ageGroup = NULL,
+                        sex = TRUE,
+                        priorHistory = TRUE,
                         tablePrefix = NULL) {
 
   ## check for standard types of user error
-
   errorMessage <- checkmate::makeAssertCollection()
 
   xCheck <- inherits(x, "tbl_dbi")
@@ -46,23 +65,20 @@ addDemographics <- function(x,
       "- cdm must be a CDMConnector CDM reference object"
     )
   }
-
   checkmate::reportAssertions(collection = errorMessage)
 
   errorMessage <- checkmate::makeAssertCollection()
-
   checkmate::assertCharacter(demographicsAt, len = 1,
                              add = errorMessage,
   )
-
   column1Check <- demographicsAt %in% colnames(x)
   if (!isTRUE(column1Check)) {
     errorMessage$push(
       "- `demographicsAt` is not a column of x"
     )
   }
-
-  # check for ageGroup, change it to a list if it is a vector of length 2, push error if other length
+  # check for ageGroup, change it to a list if it is a vector of length 2,
+  # push error if other length
   if (isTRUE(checkmate::checkIntegerish(ageGroup))) {
     if (length(ageGroup) == 2) {
       ageGroup <- list(ageGroup)
@@ -71,13 +87,11 @@ addDemographics <- function(x,
                         or a list contains multiple length two vectors")
     }
   }
-
   # after changing vector to list, we check it is a list with numeric
   checkmate::assertList(ageGroup,
                         types = "integerish", null.ok = TRUE,
                         add = errorMessage
   )
-
   # each vector in the list has to have length 2, push error if not
   if (!is.null(ageGroup)) {
     lengthsAgeGroup <- checkmate::assertTRUE(unique(lengths(ageGroup)) == 2,
@@ -87,7 +101,6 @@ addDemographics <- function(x,
       errorMessage$push("- ageGroup needs to be a numeric vector of length two,
                         or a list contains multiple length two vectors")
     }
-
     # first sort ageGroup by first value
     ageGroup <- ageGroup[order(sapply(ageGroup, function(x) x[1], simplify = TRUE), decreasing = FALSE)]
 
@@ -98,7 +111,6 @@ addDemographics <- function(x,
     checkmate::assertTRUE(all(checkAgeGroup, na.rm = TRUE),
                           add = errorMessage
     )
-
     # check ageGroup overlap
     list1 <- lapply(dplyr::lag(ageGroup), function(x) {
       x[2]
@@ -108,40 +120,47 @@ addDemographics <- function(x,
       x[1]
     })
     list2 <- unlist(list2[lengths(list2) != 0])
-
     # the first value of the interval needs to be larger than the second value of previous vector
     checkOverlap <- checkmate::assertTRUE(all(list2 - list1 > 0), add = errorMessage)
     if (!isTRUE(checkOverlap)) {
       errorMessage$push("- ageGroup can not have overlapping intervals")
     }
   }
-
   checkmate::assertCharacter(
     tablePrefix, len = 1, null.ok = TRUE, add = errorMessage
   )
-
   checkmate::reportAssertions(collection = errorMessage)
 
   # Start code
-  x <- x %>%
-    addAge(cdm, ageAt = demographicsAt) %>%
-    addSex(cdm) %>%
-    addPriorHistory(cdm, priorHistoryAt = demographicsAt)
+
+  if(isTRUE(age)){
+    x <- x %>%
+      addAge(cdm = cdm,
+             ageAt = demographicsAt,
+             defaultMonth = ageDefaultMonth,
+             defaultDay = ageDefaultDay,
+             imposeMonth = ageImposeMonth,
+             imposeDay = ageImposeDay,
+             tablePrefix = tablePrefix
+             )
+  }
   if (!is.null(ageGroup)) {
     x <- x %>%
-      addAgeGroup(cdm, ageGroup = ageGroup)
+      addAgeGroup(cdm, ageGroup = ageGroup,
+                  tablePrefix = tablePrefix)
   }
-  if(is.null(tablePrefix)){
+
+  if(isTRUE(sex)){
     x <- x %>%
-      CDMConnector::computeQuery()
-  } else {
-    x <- x %>%
-      CDMConnector::computeQuery(name = paste0(tablePrefix,
-                                               "_person_sample"),
-                                 temporary = FALSE,
-                                 schema = attr(cdm, "write_schema"),
-                                 overwrite = TRUE)
+      addSex(cdm)
   }
+
+  if(isTRUE(priorHistory)){
+    x <- x %>%
+      addPriorHistory(cdm, priorHistoryAt = demographicsAt,
+                      tablePrefix = tablePrefix)
+  }
+
   return(x)
 
 }
