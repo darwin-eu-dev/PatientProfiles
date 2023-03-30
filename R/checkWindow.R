574d76471ae01b591d470af4f53b2076fbe689b5
MimiYuchenGuo <mimimimiguo@gmail.com>
2023-03-30 17:16:15 +0100

    use cli, throw error when NA

--- R/checkWindow.R
+++ R/checkWindow.R
@@ -9,28 +9,28 @@
 #' }
 #' @noRd
 checkWindow <- function(window) {
+  # Find if any NA, throw warning that it will be changed to Inf, change it later
+  if (any(sapply(window, is.na))) {
+    cli::cli_abort("NA found in window, please change no limit to Inf ot -Inf")
+  }
+
   # if input in a single value, use it as both window start and end
   if (length(window) == 1 && lengths(window) == 1 |length(unique(unlist(window))) == 1) {
     window <- list(c(unique(unlist(window)), unique(unlist(window))))
-    warning("Only 1 window value provided, use as both window start and window end")
+    cli::cli_warn("Only 1 window value provided, use as both window start and window end")
   }
-  
-  #
+
+  # eg if list(1,2,3), change to list(c(1,1), c(2,2), c(3,3))
   if (length(window) > 1 && any(lengths(window) == 1)) {
     window[lengths(window)==1]<- lapply(window[lengths(window)==1],
                                         function(x) c(unlist(x[lengths(x)==1]),unlist(x[lengths(x)==1])))
-    warning("Window list contains element with only 1 value provided, 
+    cli::cli_warn("Window list contains element with only 1 value provided,
             use it as both window start and window end")
   }
-  
-  
+
+
   if (is.vector(window) & !is.list(window)) {
     window <- list(window)
-  }
-
-  # Find if any NA, throw warning that it will be changed to Inf, change it later
-  if (any(sapply(window, is.na))) {
-    warning("NA found in window, changed to Inf")
   }
 
 
@@ -41,13 +41,13 @@ checkWindow <- function(window) {
 
   # if any element of window list has length over 2, throw error
   if (any(lengths(window) > 2)) {
-    stop("window can only contain two values: windowStart and windowEnd")
+    cli::cli_abort("window can only contain two values: windowStart and windowEnd")
   }
 
   # change NA back to Inf
   window <- lapply(window, function(x) replace(x, is.na(x) & which(is.na(x)) == 2, Inf))
   window <- lapply(window, function(x) replace(x, is.na(x) & which(is.na(x)) == 1, -Inf))
-  
+
   checkValues <- function(x) {
     tryCatch(
       {
@@ -61,7 +61,7 @@ checkWindow <- function(window) {
   }
 
   if (any(sapply(lapply(window, checkValues), is.null))) {
-    stop("First element in window must be smaller or equal to the second one")
+    cli::cli_abort("First element in window must be smaller or equal to the second one")
   }
 
   return(window)
