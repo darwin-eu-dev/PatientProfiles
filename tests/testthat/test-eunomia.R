cdb0fc9ae624703a0e6c8deadcf7f820de246093
Xintong-Li-ZnCu <lxtlizzy@gmail.com>
2023-04-04 15:18:48 +0100

    update tests for current functions

--- tests/testthat/test-eunomia.R
+++ tests/testthat/test-eunomia.R
@@ -1,25 +1,18 @@
-test_that("test methods against eunomia duck db", {
+test_that("test addDemographics related", {
   skip_if_not(CDMConnector::eunomia_is_available())
   con <- DBI::dbConnect(duckdb::duckdb(), dbdir = CDMConnector::eunomia_dir())
   cdm <- CDMConnector::cdm_from_con(con, "main", write_schema = "main")
 
   # create a cohort table to db
   # first use of all drugs from drug_era
-  # choice A. generate from SQL.
-  # # sql <- SqlRender::readSql(system.file("sql/", "cohortsOfInterest.sql", package = "PatientProfiles"))
-  # sql <- SqlRender::readSql("./sql/cohortsOfInterest.sql")
-  # sql <- SqlRender::render(sql = sql, cdmDatabaseSchema = "main", resultsDatabaseSchema = "main")
-  # sql <- SqlRender::translate(sql = sql, targetDialect = "duckdb")
-  # DBI::dbSendQuery(con, sql)
-  # cdm <- CDMConnector::cdm_from_con(con, "main", write_schema = "main", cohort_tables = c("cohort"))
 
-  # choice B.
   cohort_table<-
     cdm$drug_era %>%
+    dplyr::filter(drug_concept_id %in% c(1125315,1112807,1713332,1759842,1729720,1177480,738818,1119510,1118084, 19127890)) %>%
     dplyr::group_by(drug_concept_id, person_id) %>%
     dplyr::summarize(cohort_definition_id = first(drug_concept_id),
-              cohort_start_date = min(drug_era_start_date),
-              cohort_end_date = min(drug_era_end_date),
+              cohort_start_date = min(drug_era_start_date,na.rm = TRUE),
+              cohort_end_date = min(drug_era_end_date,na.rm = TRUE),
               subject_id = first(person_id)) %>%
     dplyr::ungroup() %>%
     dplyr::inner_join(cdm$observation_period,
@@ -37,32 +30,24 @@ test_that("test methods against eunomia duck db", {
     expect_true(typeof(cdm$cohort_table %>% addAge(cdm) %>% dplyr::collect()) == "list")
     expect_true("age" %in% colnames(cdm$cohort_table %>% addAge(cdm)))
 
-    expect_true(typeof(cdm$condition_occurrence %>% addAge(cdm,ageAt = "condition_start_date") %>% dplyr::collect()) == "list")
-    expect_true("age" %in% colnames(cdm$condition_occurrence %>% addAge(cdm,ageAt = "condition_start_date")))
+    expect_true(typeof(cdm$condition_occurrence %>% addAge(cdm,indexDate  = "condition_start_date") %>% dplyr::collect()) == "list")
+    expect_true("age" %in% colnames(cdm$condition_occurrence %>% addAge(cdm,indexDate  = "condition_start_date")))
 
-    result <- addAge(x = cdm[["cohort_table"]], cdm = cdm, ageAt = "cohort_start_date") %>% dplyr::collect()
+    result <- addAge(x = cdm[["cohort_table"]], cdm = cdm, indexDate  = "cohort_start_date") %>% dplyr::collect()
     expect_true(all(c("cohort_definition_id", "subject_id", "cohort_start_date", "cohort_end_date", "age") %in% names(result)))
 
-  #addAgeGroup
-    expect_true(typeof(cdm$cohort_table %>% addAgeGroup(cdm = cdm) %>% dplyr::collect()) == "list")
-    expect_true("ageGroupNames" %in% colnames(cdm$cohort_table %>% addAgeGroup(cdm = cdm)))
+  #addCategories in age
+    categories <- list("age_group" = list(c(0, 18), c(19, 65), c(66,150)))
 
-    expect_true(typeof(cdm$condition_occurrence %>% addAgeGroup(cdm = cdm) %>% dplyr::collect()) == "list")
-    expect_true("ageGroupNames" %in% colnames(cdm$condition_occurrence %>% addAgeGroup(cdm = cdm)))
+    expect_true(typeof(cdm$cohort_table %>% addAge(cdm) %>%
+                         addCategories(cdm, "age", categories) %>% dplyr::collect()) == "list")
 
-    result <- addAgeGroup(
-      x = cdm$cohort_table, ageGroup = list(c(0, 18), c(19, 65), c(66,NA)), cdm = cdm
-    ) %>%
-      dplyr::collect() %>%
-      dplyr::arrange(age)
+    expect_true("age_group" %in% colnames(cdm$cohort_table %>% addAge(cdm) %>% addCategories(cdm, "age", categories)))
 
-    expect_true(all(result$ageGroupNames %in% c("0;18","19;65","66;150")))
-
-    expect_error(addAgeGroup(
-      x = cdm$cohort_table,
-      ageGroup = list(c(1, 2), c(1, 20, 30)),
-      cdm = cdm
-    ))
+    expect_true(typeof(cdm$condition_occurrence %>% addAge(cdm, indexDate = "condition_start_date") %>% addCategories(cdm, "age", categories) %>% dplyr::collect()) == "list")
+    expect_true("age_group" %in% colnames(cdm$condition_occurrence %>%
+                                            addAge(cdm, indexDate = "condition_start_date") %>%
+                                            addCategories(cdm, "age", categories)))
 
     #addSex
     expect_true(typeof(cdm$cohort_table %>% addSex(cdm) %>% dplyr::collect()) == "list")
@@ -74,42 +59,76 @@ test_that("test methods against eunomia duck db", {
     expect_true(typeof(cdm$condition_occurrence %>% addSex(cdm) %>% dplyr::collect()) == "list")
     expect_true("sex" %in% colnames(cdm$condition_occurrence %>% addSex(cdm)))
 
+
+    #addFutureObservation
+    expect_true("future_observation" %in% colnames(cdm$cohort_table %>% addFutureObservation(cdm)))
+    expect_true("future_observation" %in% colnames(cdm$condition_occurrence %>% addFutureObservation(cdm,indexDate = "condition_start_date")))
+
+    result <- cdm$cohort_table %>% addFutureObservation(cdm,indexDate  = "cohort_end_date", futureObservationName = "timeToExit")
+    expect_true("timeToExit" %in% colnames(result))
+
+
     #addDemographics
     result <- cdm$cohort_table %>%
       addDemographics(cdm,
-                      demographicsAt = "cohort_end_date",
-                      age = TRUE,
-                      ageGroup = list(c(0,100)),
-                      sex = TRUE,
-                      priorHistory = TRUE)
+                      indexDate  = "cohort_end_date",
+                      ageGroup = list("age_group" = list(c(0, 65), c(66, 120))))
     # age and age group
-    expect_true(all(c("age","sex", "ageGroupNames", "prior_history")
+    expect_true(all(c("age","sex", "age_group", "prior_history","future_observation")
                     %in%   names(result)))
 
     s <- result %>%
       dplyr::filter(
-        .data$subject_id == 1000 & .data$cohort_start_date == as.Date("2017-01-29")
-      ) %>%
+        .data$subject_id == 99 & .data$cohort_definition_id == 1119510       ) %>%
       dplyr::collect()
-    expect_true(s$age == 54)
+    expect_true(nrow(s) == 1)
+    expect_true(s$age == 57)
     expect_true(s$sex == "Female")
-    expect_true(s$prior_history == 19892)
-
-    #addFollowUp
-    result <- cdm$cohort_table %>% addFollowUp()
-    expect_true("follow_up" %in% colnames(result))
-
-    result <- cdm$condition_occurrence %>% addFollowUp(start = "condition_start_date", end = "condition_end_date")
-    expect_true("follow_up" %in% colnames(result))
-
-    result <- cdm$cohort_table %>% addFollowUp(name = "followup", start = "cohort_end_date")
-    expect_true("followup" %in% colnames(result))
-    expect_true(all(1 == result %>%
-                      dplyr::select(followup) %>% dplyr::pull()))
-
-    #addCohortIntersect
-
+    expect_true(s$prior_history == 20615)
 
     DBI::dbDisconnect(attr(cdm, "dbcon"), shutdown = TRUE)
 
   })
+
+test_that("test addIntersect related", {
+  skip_if_not(CDMConnector::eunomia_is_available())
+  con <- DBI::dbConnect(duckdb::duckdb(), dbdir = CDMConnector::eunomia_dir())
+  cdm <- CDMConnector::cdm_from_con(con, "main", write_schema = "main")
+
+  # create a cohort table to db
+  # first use of all drugs from drug_era
+
+  #countOccurrences
+  cohort_table<-
+    cdm$drug_era %>%
+    dplyr::filter(drug_concept_id %in% c(1713332,1759842,1729720,1177480 )) %>%
+    dplyr::group_by(drug_concept_id, person_id) %>%
+    dplyr::summarize(cohort_definition_id = first(drug_concept_id),
+                     cohort_start_date = min(drug_era_start_date,na.rm = TRUE),
+                     cohort_end_date = min(drug_era_end_date,na.rm = TRUE),
+                     subject_id = first(person_id)) %>%
+    dplyr::ungroup() %>%
+    dplyr::inner_join(cdm$observation_period,
+                      by = c("subject_id" = "person_id")) %>%
+    dplyr::filter(cohort_start_date >= observation_period_start_date,
+                  cohort_end_date <= observation_period_end_date) %>%
+    dplyr::select(cohort_definition_id, cohort_start_date, cohort_end_date , subject_id)
+
+
+  cohort_table_1 <- cohort_table %>% dplyr::filter(cohort_definition_id %in% c(1713332,1759842 ))  %>% dplyr::collect()
+  cohort_table_2 <- cohort_table %>% dplyr::filter(cohort_definition_id %in% c(1729720,1177480 ))  %>% dplyr::collect()
+
+  DBI::dbWriteTable(con, "cohort_table_1", cohort_table_1, overwrite = TRUE)
+  DBI::dbWriteTable(con, "cohort_table_2", cohort_table_2, overwrite = TRUE)
+
+  cdm <- CDMConnector::cdm_from_con(con, "main", write_schema = "main", cohort_tables = c("cohort_table_1","cohort_table_2") )
+
+  result <- cdm$cohort_table_1 %>%
+    countOccurrences(cdm = cdm, tableName = "cohort_table_2",cohortId = 1729720 ,window = list(c(0, -Inf))) %>%
+    dplyr::arrange(subject_id, cohort_start_date) %>%
+    dplyr::collect()
+
+
+  DBI::dbDisconnect(attr(cdm, "dbcon"), shutdown = TRUE)
+
+})
