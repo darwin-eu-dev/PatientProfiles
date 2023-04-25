2fbfdf76254432516e5d79c737754833df4a909d
catalamarti <marticatalasabate@gmail.com>
2023-04-25 18:06:59 +0100

    remove warnings

--- tests/testthat/test-addIntersect.R
+++ tests/testthat/test-addIntersect.R
@@ -365,9 +365,9 @@ test_that("working examples with extra column", {
     dplyr::arrange(subject_id, cohort_start_date) %>%
     dplyr::collect()
 
-  expect_true(all(result$measurment_result_covid_0_to_Inf == c(1,1,2,6,5) ))
+  expect_true(all(result$measurment_result_covid_0_to_inf == c(1,1,2,6,5) ))
   # expect_true(all(result_1$measurment_result_covid_0_to_Inf == c("NA","NA","NA","NA","NA") )) # to be updated when implemented
-  expect_true(all(result_2$measurment_result_0_to_Inf == c("1; 2","1; 2",3,6,5) ))
+  expect_true(all(result_2$measurment_result_0_to_inf == c("1; 2","1; 2",3,6,5) ))
   expect_true(all(is.na(result_3$measurment_result_m400_to_m200 )))
 
   DBI::dbDisconnect(attr(cdm, "dbcon"), shutdown = TRUE)
