642fb672c5273ef51c20ae561c0727f456dac6a2
catalamarti <marticatalasabate@gmail.com>
2023-05-24 17:17:32 +0100

    simplify

--- tests/testthat/test-summariseCharacteristics.R
+++ tests/testthat/test-summariseCharacteristics.R
@@ -7,5 +7,5 @@ test_that("test all functions", {
     v4 = as.Date(c("2021-05-12", "2012-05-15", "2023-11-30", "2015-12-10", "2014-01-12"))
   )
   summariseCharacteristics(x)
-  summariseCharacteristics(x, strata = list("group" = "s")) %>% print(n = 500)
+  summariseCharacteristics(x, strata = list("group" = "s"), suppressCellCount = 1) %>% print(n = 500)
 })
