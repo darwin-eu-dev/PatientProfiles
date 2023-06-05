4c34680115ca3df6b142794f2c2151c85c3d6201
catalamarti <marticatalasabate@gmail.com>
2023-06-05 19:38:37 +0100

    add tests on redshift

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,6 +6,17 @@ availableConnections <- list(
     con = DBI::dbConnect(duckdb::duckdb(), ":memory:"),
     scratch_schema = "main",
     write_schema = "main"
+  ),
+  redshift <- list(
+    con = DBI::dbConnect(
+      RPostgres::Redshift(), dbname = Sys.getenv("CDM5_REDSHIFT_DBNAME"),
+      port = Sys.getenv("CDM5_REDSHIFT_PORT"),
+      host = Sys.getenv("CDM5_REDSHIFT_HOST"),
+      user = Sys.getenv("CDM5_REDSHIFT_USER"),
+      password = Sys.getenv("CDM5_REDSHIFT_PASSWORD")
+    ),
+    scratch_schema = Sys.getenv("CDM5_REDSHIFT_SCRATCH_SCHEMA"),
+    write_schema = Sys.getenv("CDM5_REDSHIFT_OHDSI_SCHEMA")
   )
 )
 for (k in seq_along(availableConnections)) {
