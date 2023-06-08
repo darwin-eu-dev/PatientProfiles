2d13aac87aba061bba14522c640cea5a1bea739a
catalamarti <marticatalasabate@gmail.com>
2023-06-08 19:14:42 +0100

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -23,6 +23,25 @@ availableConnections <- list(list(
 #     )))
 # }
 
+if (Sys.getenv("CDM5_SQL_SERVER_USER") != "") {
+  availableConnections <- availableConnections %>%
+    append(value = list(list(
+      con = DBI::dbConnect(
+        odbc::odbc(),
+        Driver   = "ODBC Driver 18 for SQL Server",
+        Server   = Sys.getenv("CDM5_SQL_SERVER_SERVER"),
+        Database = Sys.getenv("CDM5_SQL_SERVER_CDM_DATABASE"),
+        UID      = Sys.getenv("CDM5_SQL_SERVER_USER"),
+        PWD      = Sys.getenv("CDM5_SQL_SERVER_PASSWORD"),
+        TrustServerCertificate = "yes",
+        Port     = 1433
+      ),
+      scratch_schema = Sys.getenv("CDM5_SQL_SERVER_SCRATCH_SCHEMA"),
+      write_schema = Sys.getenv("CDM5_SQL_SERVER_OHDSI_SCHEMA")
+    )))
+}
+
+
 for (k in seq_along(availableConnections)) {
   connectionDetails <- availableConnections[[k]]
   test_check("PatientProfiles")
