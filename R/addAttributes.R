f93644c2626987d6ed553cf98168f3ec4175d677
catalamarti <marticatalasabate@gmail.com>
2023-06-16 18:03:55 +0100

    add cohort name

--- R/addAttributes.R
+++ R/addAttributes.R
@@ -42,3 +42,28 @@ addAttributes <- function(newcohort,
 
   return(newcohort)
 }
+
+#' Add cohort name for each cohort_definition_id
+#'
+#' @param cohort cohort to which add the cohort name
+#'
+#' @return cohort with an extra column with the cohort names
+#'
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' library(PatientProfiles)
+#'
+#' cdm <- mockPatientProfiles()
+#' cdm$cohort1 %>%
+#'   addCohortName()
+#' }
+#'
+addCohortName <- function(cohort) {
+  cohort %>%
+    dplyr::left_join(
+      CDMConnector::cohortSet(cohort), by = "cohort_definition_id",
+      copy = TRUE
+    )
+}
