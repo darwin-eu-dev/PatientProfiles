09b8fc95e825f1e0b3d42d94ae09fef47c10626a
catalamarti <marticatalasabate@gmail.com>
2023-09-11 01:07:32 +0100

    structure

--- R/summariseLargeScaleCharacteristics.R
+++ R/summariseLargeScaleCharacteristics.R
@@ -28,8 +28,6 @@
 #' @param overlapAtc ATC levels to characterise.
 #' @param incidentIcd10 ICD10 levels to characterise.
 #' @param overlapIcd10 ICD10 levels to characterise.
-#' @param incidentConceptSet Concept sets to characterise.
-#' @param overlapConceptSet Concept sets to characterise.
 #' @param minCellCount All counts lower than minCellCount will be obscured.
 #' @param cdm A cdm reference.
 #'
@@ -67,8 +65,112 @@ summariseLargeScaleCharacteristics <- function(cohort,
                                                overlapAtc = NULL,
                                                incidentIcd10 = NULL,
                                                overlapIcd10 = NULL,
-                                               incidentConceptSet = NULL,
-                                               overlapConceptSet = NULL,
                                                minCellCount = 5,
                                                cdm = attr(cohort, "cdm_reference")) {
+  # edit write prefix
+  writeSchema <- attr(attr(cdm, "cdm_reference"), "write_schema")
+  if ("prefix" %in% names(writeSchema)) {
+    writeSchema["prefix"] <- paste0(writeSchema["prefix"], "lsc_")
+  } else {
+    writeSchema["prefix"] <- "lsc_"
+  }
+
+  # initial table
+  x <- cohort %>%
+    dplyr::select("subejct_id", "cohort_start_date") %>%
+    dplyr::distinct() %>%
+    dplyr::group_by(.data$subject_id) %>%
+    dbplyr::window_order(.data$cohort_start_date) %>%
+    dplyr::mutate(obs_id = dplyr::row_number()) %>%
+    CDMConnector::computeQuery(
+      name = "individuals", temporary = FALSE, schema = writeSchema,
+      overwrite = TRUE
+    )
+
+  # get analysis table
+  analysis <- list(
+    dplyr::tibble(
+      table = incidentStandard, event = "incident", concept = "standard",
+      name = "standard"
+    ),
+    dplyr::tibble(
+      table = overlapStandard, event = "overlap", concept = "standard",
+      name = "standard"
+    ),
+    dplyr::tibble(
+      table = incidentSource, event = "incident", concept = "source",
+      name = "source"
+    ),
+    dplyr::tibble(
+      table = overlapSource, event = "overlap", concept = "source",
+      name = "source"
+    ),
+    dplyr::tibble(
+      table = "drug_exposure", event = "incident", concept = "standard",
+      name = incidentAtc
+    ),
+    dplyr::tibble(
+      table = "drug_exposure", event = "overlap", concept = "standard",
+      name = overlapAtc
+    ),
+    dplyr::tibble(
+      table = "condition_occurrence", event = "incident", concept = "standard",
+      name = incidentIcd10
+    ),
+    dplyr::tibble(
+      table = "condition_occurrence", event = "overlap", concept = "standard",
+      name = overlapIcd10
+    )
+  ) %>%
+    dplyr::bind_rows() %>%
+    tidyr::drop_na()
+
+  # perform lsc
+  lsc <- NULL
+  for (tab in unique(analysis$table)) {
+    analysisTable <- analysis %>% dplyr::filter(.data$table == .env$tab)
+    toSelect <- c(
+      "subject_id" = "person_id",
+      "start" = getStartName(tab),
+      ifelse(is.na(getEndName(tab)), NULL, "end" = getEndName(tab)),
+      ifelse(
+        "standard" %in% analysisTable$concept,
+        "standard" = getConceptName(tab), NULL
+      ),
+      ifelse(
+        "source" %in% analysisTable$concept,
+        "source" = getSourceConceptName(tab), NULL
+      )
+    )
+    table <- cdm[[tab]] %>%
+      dplyr::select(dplyr::all_of(toSelect)) %>%
+      dplyr::inner_join(x, by = "subject_id") %>%
+      dplyr::mutate(
+        start = !!CDMConnector::datediff("cohort_start_date", "start")
+      )
+    if ("end" %in% names(toSelect)) {
+      table <- table %>%
+        dplyr::mutate(
+          end = !!CDMConnector::datediff("cohort_start_date", "end")
+        )
+    }
+    table <- table %>%
+      CDMConnector::computeQuery(
+        name = "table", temporary = FALSE, schema = writeSchema,
+        overwrite = TRUE
+      )
+    for (k in seq_len(nrow(analysisTable))) {
+      lsc <- lsc %>%
+        dplyr::union_all(
+          getLsc(x, table, tab, strata, window, analysisTable[k, ])
+        )
+    }
+  }
+
+  # suppress counts
+  lsc <- suppressCounts(lsc)
+
+  # return
+  return(lsc)
 }
+
