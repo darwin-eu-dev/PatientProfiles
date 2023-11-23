d96d6f161d61a73c2fb41a1916920fa0ee545226
catalamarti <marticatalasabate@gmail.com>
2023-11-23 12:16:35 +0000

    failing test

--- tests/testthat/test-addCohortIntersect.R
+++ tests/testthat/test-addCohortIntersect.R
@@ -768,4 +768,15 @@ test_that("censorDate functionality", {
       unlist(use.names = F),
     c(0, 0, NA, NA)
   )))
+
+
+
+})
+
+test_that("casing of empty dates", {
+  cdm <- mockPatientProfiles(connectionDetails)
+  cdm$cohort1 <- cdm$cohort1 %>% dplyr::filter(cohort_definition_id == 1)
+  cdm$cohort2 %>%
+    addCohortIntersectDate(targetCohortTable = "cohort1") %>%
+    dplyr::pull("")
 })
