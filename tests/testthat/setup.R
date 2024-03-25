362027c05af0c628636a704c6cd8aa99b9f237a8
catalamarti <marticatalasabate@gmail.com>
2024-03-25 17:40:52 +0000

    download eunomia

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,3 +1,9 @@
+withr::local_envvar(
+  R_USER_CACHE_DIR = tempfile(),
+  .local_envir = teardown_env(),
+  EUNOMIA_DATA_FOLDER = Sys.getenv("EUNOMIA_DATA_FOLDER", unset = tempfile())
+)
+downloadEunomiaData(overwrite = TRUE)
 connection <- function(dbToTest) {
   switch(
     dbToTest,
