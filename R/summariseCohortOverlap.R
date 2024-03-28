bc7c157e4bd0293d7e74c4a0b0435a69f419ddc4
catalamarti <marticatalasabate@gmail.com>
2024-03-28 11:39:02 +0000

    note

--- R/summariseCohortOverlap.R
+++ R/summariseCohortOverlap.R
@@ -68,7 +68,7 @@ summariseCohortOverlap <- function(cohort,
   cohort_counts <- omopgenerics::cohortCount(cdm[[name]]) |>
     dplyr::inner_join(omopgenerics::settings(cdm[[name]]),
                       by = "cohort_definition_id") |>
-    dplyr::select(cohort_name, number_subjects)
+    dplyr::select("cohort_name", "number_subjects")
   # get inner join counts
   overlap <- overlapOverallData |>
     dplyr::group_by(.data$cohort_name_reference,
