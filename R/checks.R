7ae63a5ea529352a8a58897c832ae246d9306797
catalamarti <marticatalasabate@gmail.com>
2024-04-02 15:33:41 +0100

    improve error message for strata #483

--- R/checks.R
+++ R/checks.R
@@ -417,7 +417,13 @@ checkStrata <- function(list, table, type = "strata") {
       cli::cli_abort(errorMessage)
     }
     if (!all(unlist(list) %in% colnames(table))) {
-      cli::cli_abort(errorMessage)
+      notPresent <- list |> unlist() |> unique()
+      notPresent <- notPresent[! notPresent %in% colnames(table)]
+      cli::cli_abort(paste0(
+        errorMessage,
+        ". The following columns were not found in the data: ",
+        paste0(notPresent, collapse = ", ")
+      ))
     }
   }
 }
